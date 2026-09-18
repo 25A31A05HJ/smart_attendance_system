12e44b882ef861c08eb7eb2e2acf42c50636484c
VEERA PURNIMA VENKATA <purnimaveera678@gmail.com>
2026-09-18 10:07:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -323,4 +323,4 @@ def attendance():
 # ==========================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
